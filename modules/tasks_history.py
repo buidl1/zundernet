c90bdab6835634b55e8acd0576bb1ff89dd801cc
buidl1 <71228185+buidl1@users.noreply.github.com>
2021-03-07 13:31:41 +0100

    Update tasks_history.py

--- modules/tasks_history.py
+++ modules/tasks_history.py
@@ -1,14 +1,13 @@
 
-import tkinter as tk
-from tkinter import filedialog, StringVar, ttk, messagebox, Toplevel 
+
 import os,sys
 import datetime
 import json
 import time
 import modules.localdb as localdb
 import modules.app_fun as app_fun
-# import operator
-import modules.flexitable as flexitable
+
+import modules.gui as gui
 
 class TasksHistory:
 
@@ -16,83 +15,66 @@ class TasksHistory:
 		idb=localdb.DB()
 		task_done=idb.select('queue_done', ['command'],distinct=True)
 		tmpcommands=[cc[0] for cc in task_done]
+		colnames=['Category','Command','Last','Result']
 		tmpdict={}
-		tmpdict['filters']=[{'T':'LabelC', 'L':'Category: '}
-							, {'T':'Combox', 'uid':'category', 'V':['All','manual','auto'] }
-							, {'T':'LabelC', 'L':'Command: '}
-							, {'T':'Combox', 'uid':'command', 'V':['All']+tmpcommands }
-							, {'T':'LabelC', 'L':'Last: '}
-							, {'T':'Combox', 'uid':'last', 'V':['24h','week','month','12 months','All'] }
-							, {'T':'LabelC', 'L':'Result: '}
-							, {'T':'Combox', 'uid':'result', 'V':['All','Failed','Success'] }
+		tmpdict['rowk']='filters'
+		tmpdict['rowv']=[ {'T':'Combox',  'V':['All','manual','auto'] }
+							, {'T':'Combox', 'V':['All']+tmpcommands }
+							, {'T':'Combox', 'V':['24h','week','month','12 months','All'] }
+							, {'T':'Combox',  'V':['All','Failed','Success'] }
 							]
 		grid_filter=[]
 		grid_filter.append(tmpdict )
 		
-		self.filter_table.update_frame(grid_filter,head_offset=-1)
+		self.filter_table.updateTable(grid_filter,colnames)    #update_frame(grid_filter,head_offset=-1)
 		
 	def update_history_frame(self,*eventargs):
 		if not self.update_in_progress:
 			self.update_in_progress=True
-			self.grid_settings=[]
+			# self.grid_settings=[]
 			self.update_list()
 			
-			self.main_table.update_frame(self.grid_settings)
+			# self.main_table.update_frame(self.grid_settings)
+			self.main_table.updateTable(self.grid_settings,self.colnames)
 			self.update_filter_cmd()
 			self.update_in_progress=False
 		
+		
+		
+		
 
-	def __init__(self,parent_frame):
+	def __init__(self ):
 		self.grid_settings=[]
 		self.update_in_progress=False
+		self.parent_frame = gui.ContainerWidget(None,layout=gui.QVBoxLayout() )
 
-		idb=localdb.DB()
+		frame0=gui.FramedWidgets(None,'Filter') #ttk.LabelFrame(parent_frame,text='Filter') # 
+		frame0.setMaximumHeight(128)
+		self.parent_frame.insertWidget(frame0)
 		
-		frame0=ttk.LabelFrame(parent_frame,text='Filter') #
-		frame0.grid(row=0,column=0, sticky="nsew")
 		
-		task_done=idb.select('queue_done', ['command'],distinct=True)
-		tmpcommands=[cc[0] for cc in task_done]
-		tmpdict={}
-		tmpdict['filters']=[{'T':'LabelC', 'L':'Category: '}
-							, {'T':'Combox', 'uid':'category', 'V':['All','manual','auto'] }
-							, {'T':'LabelC', 'L':'Command: '}
-							, {'T':'Combox', 'uid':'command', 'V':['All']+tmpcommands }
-							, {'T':'LabelC', 'L':'Last: '}
-							, {'T':'Combox', 'uid':'last', 'V':['24h','week','month','12 months','All'] }
-							, {'T':'LabelC', 'L':'Result: '}
-							, {'T':'Combox', 'uid':'result', 'V':['All','Failed','Success','Cancelled'] }
-							]
-						
-		grid_filter=[]
-		grid_filter.append(tmpdict )
+		self.filter_table=gui.Table(None,params={'dim':[1,4],'updatable':1} )  #flexitable.FlexiTable(frame0,grid_filter) 
+		frame0.insertWidget(self.filter_table)
+		self.update_filter_cmd()
 		
-		self.filter_table=flexitable.FlexiTable(frame0,grid_filter) 
-		# addr book view left:
-		frame1=ttk.LabelFrame(parent_frame,text='Tasks list') 
-		frame1.grid(row=1,column=0, sticky="nsew")
+		frame1=gui.FramedWidgets(None,'Tasks list')    #ttk.LabelFrame(parent_frame,text='Tasks list') 
+		self.parent_frame.insertWidget(frame1)
+		# frame1.grid(row=1,column=0, sticky="nsew")
 			
 		tmpdict={}
-		tmpdict['head']=[{'T':'LabelC', 'L':'Category' } , 
-						{'T':'LabelC', 'L':'Command' } , 
-						{'T':'LabelC', 'L':'Command json' } , 
-						{'T':'LabelC', 'L':'Created at' } , 
-						{'T':'LabelC', 'L':'Finished at' } , 
-						{'T':'LabelC', 'L':'Result' } , 
-						{'T':'LabelC', 'L':'wait_seconds' } , 
-						{'T':'LabelC', 'L':'Result details' }
-						]		
-						
-		self.grid_settings.append(tmpdict)
 		
+		self.colnames=['Category','Command','Command json','Created at','Finished at','Result','wait_seconds','Result details']
+			
 		self.update_list()
 		
-		self.main_table=flexitable.FlexiTable(frame1,self.grid_settings, min_canvas_width=1200,force_scroll=True)
+		self.main_table=gui.Table(None,params={'dim':[len(self.grid_settings),len(self.colnames)],'updatable':1} )  #flexitable.FlexiTable(frame0,grid_filter) 
+		frame1.insertWidget(self.main_table)    #flexitable.FlexiTable(frame1,self.grid_settings, min_canvas_width=1200,force_scroll=True)
+		self.main_table.updateTable(self.grid_settings,self.colnames)
 		
-		self.filter_table.bind_combox_cmd('category',[], self.update_history_frame )	
-		self.filter_table.bind_combox_cmd('result',[],  self.update_history_frame )	
-		self.filter_table.bind_combox_cmd('command',[],  self.update_history_frame )	
-		self.filter_table.bind_combox_cmd('last',[],  self.update_history_frame )
+		self.filter_table.cellWidget(0,0).set_fun(self.update_history_frame )    
+		self.filter_table.cellWidget(0,1).set_fun(self.update_history_frame) #.bind_combox_cmd('result',[],  self.update_history_frame )	
+		self.filter_table.cellWidget(0,2).set_fun(self.update_history_frame) #.bind_combox_cmd('command',[],  self.update_history_frame )	
+		self.filter_table.cellWidget(0,3).set_fun(self.update_history_frame) #.bind_combox_cmd('last',[],  self.update_history_frame )
 	
 	
 	def update_list(self):
@@ -100,7 +82,7 @@ class TasksHistory:
 		idb=localdb.DB()
 		
 		wwhere={}
-		llast=self.filter_table.get_value('last')
+		llast=self.filter_table.cellWidget(0,2).currentText() #.get_value('last')
 		if llast=='24h':  
 			wwhere['datetime(created_time)']=['>=',"datetime('"+app_fun.date2str(datetime.datetime.now()-datetime.timedelta(hours=24) )+"')"]
 		elif llast=='week':  
@@ -112,11 +94,11 @@ class TasksHistory:
 		
 		task_done=idb.select('queue_done', ['type','command','json','created_time','end_time','result','wait_seconds','id'],where=wwhere,orderby=[{'end_time':'desc'},{'created_time':'desc'}])
 		
-		ccat=self.filter_table.get_value('category')
-		rres=self.filter_table.get_value('result')
-		cmd=self.filter_table.get_value('command')
+		ccat=self.filter_table.cellWidget(0,0).currentText() #get_value('category')
+		rres=self.filter_table.cellWidget(0,3).currentText() #.get_value('result')
+		cmd=self.filter_table.cellWidget(0,1).currentText() #.get_value('command')
 		
-
+		self.grid_settings=[]
 	
 		for rr in task_done:
 			tmpdict={}
@@ -125,8 +107,8 @@ class TasksHistory:
 			if 'error' in rr[5].lower() or 'failed' in rr[5].lower() or 'false' in rr[5].lower():
 				short_result='Failed/False'
 				sstyle={'bgc':'red','fgc':'black'}
-			elif 'cancelled' in rr[5].lower():
-				short_result='Cancelled'
+			elif 'canceled' in rr[5].lower():
+				short_result='Canceled'
 				sstyle={'bgc':'blue','fgc':'white'}
 				
 			visible=True
@@ -141,7 +123,8 @@ class TasksHistory:
 				# visible=False
 				continue
 				
-			tmpdict[rr[7]]=[{'T':'LabelV', 'L':rr[0], 'uid':'cat'+str(rr[7]), 'visible':visible } , 
+			tmpdict['rowk']=rr[7]
+			tmpdict['rowv']=[{'T':'LabelV', 'L':rr[0], 'uid':'cat'+str(rr[7]), 'visible':visible } , 
 							{'T':'LabelV', 'L':rr[1], 'uid':'cmd'+str(rr[7]) , 'visible':visible} , 
 							{'T':'InputL', 'L':rr[2], 'uid':'json'+str(rr[7]) , 'visible':visible, 'width':24} , 
 							{'T':'LabelV', 'L':rr[3], 'uid':'ctime'+str(rr[7]) , 'visible':visible} , 
