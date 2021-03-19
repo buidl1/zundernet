66889fb3b4e4af13bd0e4fef2799ee5d47f77b7b
buidl1 <71228185+buidl1@users.noreply.github.com>
2021-03-19 19:40:58 +0100

    Update init_checks.py

--- modules/init_checks.py
+++ modules/init_checks.py
@@ -12,6 +12,7 @@ import modules.aes as aes
 import modules.gui as gui
 import modules.deamon as deamon
 import modules.usb as usb
+import getpass
 
 # props:
 # self.cc=aes.Crypto()
@@ -24,8 +25,14 @@ class InitApp:
 
 	def deamon_setup(self,tt):
 		
-		dpath= tt[0][0]+'/komodod'
-		cpath=tt[0][0]+'/komodo-cli'
+		pirated=False
+		dpath=os.path.join(tt[0][0],'komodod')
+		cpath=os.path.join(tt[0][0],'komodo-cli')
+		if not os.path.exists(dpath):
+			dpath=os.path.join(tt[0][0],'pirated')
+			cpath=os.path.join(tt[0][0],'pirate-cli')
+			pirated=True
+		
 		ddatap=tt[0][1]
 		
 		if sys.platform=='win32':
@@ -35,11 +42,23 @@ class InitApp:
 		deamon_cfg={
 			"deamon-path":dpath, 
 			"cli-path":cpath, 
+			"wallet":self.wallet,
 			"ac_name": "PIRATE",
 			"ac_params":"-ac_supply=0 -ac_reward=25600000000 -ac_halving=77777 -ac_private=1", # -rescan if needed
 			"datadir":ddatap, 
 			"fee":"0.0001"# "addnode":["136.243.102.225", "78.47.205.239"],
 		}	
+		
+		if pirated:
+			deamon_cfg={
+				"deamon-path":dpath, 
+				"cli-path":cpath, 
+				"wallet":self.wallet,
+				"datadir":ddatap, 
+				"fee":"0.0001"# "addnode":["136.243.102.225", "78.47.205.239"],
+			}
+			
+		# print(deamon_cfg)
 		
 		return deamon_cfg
 
@@ -47,9 +66,12 @@ class InitApp:
 		
 		
 		
-		
+	# need to generate name here for config and save it 	
 	def __init__(self):
-	
+		self.wallet ='tmpwallet.dat'
+		# self.data_files['wallet']
+		self.data_files={'wallet':'wallet','db':'local_storage'} # .dat/.encr or .db/.encr
+		
 		self.close_thread=False
 
 		app_fun.check_already_running(os.path.basename(__file__)) # check app not runnin - else escape !
@@ -60,14 +82,17 @@ class InitApp:
 		
 		is_deamon_working=app_fun.check_deamon_running()
 		idb=localdb.DB('init.db')
-		db=localdb.DB( )
+		# db=localdb.DB( )
 
-		tt= idb.select('init_settings',columns=["komodo","datadir","start_chain"]) 
+		tt= idb.select('init_settings',columns=["komodo","datadir","data_files"]) 
 		
 		if is_deamon_working[0]:
 			if len(tt)==0:
 				gui.messagebox_showinfo("init.db file missing while running - exit", "init.db file missing while running - exit")
 				exit()
+			# print(tt)	
+			tmp=json.loads(tt[0][2])
+			self.data_files={ 'wallet':tmp['wallet'],'db':tmp['db'] }
 
 		else: # deamon starting - ask paths 
 			self.paths_confirmed=False
@@ -76,17 +101,18 @@ class InitApp:
 				gui.messagebox_showinfo("Exiting app...", "Exiting app...")
 				exit()
 			
-			tt= idb.select('init_settings',columns=["komodo","datadir","start_chain"])  
+			tt= idb.select('init_settings',columns=["komodo","datadir", "data_files"])  
 			if len(tt)==0 :
 				gui.messagebox_showinfo("init.db file missing while running - exit", "init.db file missing while running - exit")
 				exit()
+				
+			# idb.upsert(dict_set,["lock"],{})
 
-		self.autostart=tt[0][2]
+		# self.autostart=tt[0][2]
+		self.autostart='no'	
 		if is_deamon_working[0]:
 			self.autostart='yes'	
-
 			
-		deamon_cfg=self.deamon_setup(tt)
 		dict_set={}
 		dict_set['lock_db_threads']=[{'lock':'no'}]
 		if idb.check_table_exist('lock_db_threads'):
@@ -95,9 +121,10 @@ class InitApp:
 		self.first_run=False
 		
 		ppath=os.getcwd()	
-		if os.path.exists(os.path.join(ppath , 'local_storage.encr'))==False and os.path.exists(os.path.join(ppath , 'local_storage.db'))==False:
-			gui.messagebox_showinfo("Creating new local_storage.db","Creating new local_storage.db")
-			localdb.init_tables()
+		if os.path.exists(os.path.join(ppath , self.data_files['db']+'.encr'))==False and os.path.exists(os.path.join(ppath ,self.data_files['db']+'.db'))==False:
+			newdbfname= self.data_files['db']+'.db' 
+			gui.messagebox_showinfo("Creating new database","Creating new database file:\n"+newdbfname)
+			localdb.init_tables(newdbfname)
 			self.first_run=True
 
 		self.app_password=None
@@ -119,40 +146,66 @@ class InitApp:
 			
 		###################### INIT DB AND DEAMON	
 		if not self.first_run:
-			localdb.init_tables()
+			localdb.init_tables(self.data_files['db']+'.db')
 		
-		self.dmn=deamon.DeamonInit(deamon_cfg)
+		self.wallet = self.data_files['wallet']+'.dat'
+		self.db = self.data_files['db']+'.db'
+		
+		deamon_cfg=self.deamon_setup(tt)
+		self.dmn=deamon.DeamonInit(deamon_cfg,self.db)
 		self.dmn.init_clear_queue()	
+		# self.data_files={ 'wallet':tmp['wallet'],'db':tmp['local_storage'] }
+		
 		
 		
 		
 		
 		
 	
-	def update_paths(self,deamon,data,chain,parent):
+	def update_paths(self,deamon,data,str_data_files,parent,new_wallet):
+	
 		idb=localdb.DB('init.db')
 		komodod_ok=False
-		if os.path.exists( os.path.join(deamon,'komodod.exe') ) or os.path.exists(deamon+'/komodod'):
-			komodod_ok=True
-			
+		allowed_deamons=['komodod.exe','komodod','pirated','pirated.exe']
+		for ad in allowed_deamons:
+			if os.path.exists( os.path.join(deamon,ad) ):
+				komodod_ok=True
+				break
+				
 		komodo_cli_ok=False
-		if os.path.exists( os.path.join(deamon,'komodo-cli.exe') ) or os.path.exists(deamon+'/komodo-cli'):
-			komodo_cli_ok=True
+		allowed_cli=['komodo-cli.exe','komodo-cli','pirate-cli','pirate-cli.exe']
+		for ac in allowed_cli:
+			if os.path.exists( os.path.join(deamon,ac) ):
+				komodo_cli_ok=True
+				break		
+				
+		
+		# if os.path.exists( os.path.join(deamon,'komodod.exe') ) or os.path.exists(deamon+'/komodod') or os.path.exists( os.path.join(deamon,'pirated.exe') ) or os.path.exists(deamon+'/pirated'):
+			# komodod_ok=True
+			
+		# if os.path.exists( os.path.join(deamon,'komodo-cli.exe') ) or os.path.exists(deamon+'/komodo-cli'):
+			# komodo_cli_ok=True
 			
 		blockchain_data_ok=False
-		decr_wal_exist=os.path.exists( os.path.join(data,'wallet.dat'))
-		if decr_wal_exist or os.path.exists( os.path.join(data,'wallet.encr') ):
+		decr_wal_exist=app_fun.fileExist(data,cond={'start':'wallet','end':'.dat' })
+		if decr_wal_exist or app_fun.fileExist(data,cond={'start':'wallet','end':'.encr' }):
 			blockchain_data_ok=True
+		
+		# decr_wal_exist=os.path.exists( os.path.join(data,self.data_files['wallet']+'.dat'))
+		# if decr_wal_exist or os.path.exists( os.path.join(data,self.data_files['wallet']+'.encr') ):
+			# blockchain_data_ok=True
 			
-			
+		print('blockchain_data_ok',blockchain_data_ok,new_wallet)
 			
 			
 		if decr_wal_exist: # backup!
-		
+			
+			gui.messagebox_showinfo('Wallet backup required','Decrypted wallet exist - backup required')
+			
 			uu=usb.USB()
 			
 			while len(uu.locate_usb())==0:
-				gui.messagebox_showinfo('Please insert USB pendrive','Please insert USB pendrive. Unencrypted wallet.dat file detected - needs to be backed up to external memory.')
+				gui.messagebox_showinfo('Please insert USB pendrive','Please insert USB pendrive. Unencrypted wallet file detected - needs to be backed up to external memory.')
 				
 			path=uu.locate_usb()
 			path=path[0]
@@ -171,47 +224,55 @@ class InitApp:
 				
 				# path=filedialog.askdirectory(initialdir=tmpinitdir, title="Select directory on your USB drive") # was 
 				path2=gui.set_file( None,None,True,parent,init_path=tmpinitdir,title="Select directory on your USB drive" )
-				
-				if uu.verify_path_is_usb(path2):
-					# gui.messagebox_showinfo('Starting backup','Please wait untill backup is finished and relevant message is displayed.' )
-					dest=os.path.join(path2,'wallet_'+app_fun.now_to_str()+'.dat')  
-					src= os.path.join(data,'wallet.dat')
-					
-					path2=gui.copy_progress(path2, 'Wallet backup to '+path2+'\n',src,dest)
-					
-					# try:
-						# shutil.copy(src, dest)
-						# path2=''
-						# gui.messagebox_showinfo('Backup done','Your wallet is safe now at \n'+dest )
-						# break
-					# except:
-						# exit()
+				# print(path2)
+				if path2!=None:
+					if uu.verify_path_is_usb(path2):
+						# gui.messagebox_showinfo('Starting backup','Please wait untill backup is finished and relevant message is displayed.' )
+						dest=os.path.join(path2,self.data_files['wallet']+ '_'+app_fun.now_to_str()+'.dat')  
+						src= os.path.join(data,self.data_files['wallet']+'.dat')
+						
+						path2=gui.copy_progress(path2, 'Wallet backup to '+path2+'\n',src,dest)
+						
+						# try:
+							# shutil.copy(src, dest)
+							# path2=''
+							# gui.messagebox_showinfo('Backup done','Your wallet is safe now at \n'+dest )
+							# break
+						# except:
+							# exit()
+					else:
+						gui.messagebox_showinfo('Wrong path','Selected path is not USB drive, please try again.' )
+						path2=''
 				else:
-					gui.messagebox_showinfo('Wrong path','Selected path is not USB drive, please try again.' )
-					path2=''
+					# path2=''
+					exit()
 		
 		
 		
 		
 		
-		if komodod_ok and komodo_cli_ok and blockchain_data_ok:  
+		if komodod_ok and komodo_cli_ok  and (blockchain_data_ok or new_wallet):  
 
+			# wallet name = wallet_ABECUP.dat local_storage_ABECUP.dat
+		
+			# data_files=json.dumps()
+		
 			dict_set={}
 			dict_set['init_settings']=[]
 			dict_set['init_settings'].append({
 												"komodo":  deamon ,
-												"datadir": data ,
-												"start_chain":chain
+												"datadir": data  ,
+												"data_files":str_data_files
 											})
 			
 			idb.delete_where('init_settings')
-			idb.insert(dict_set,["komodo","datadir","start_chain"]) #,"password_on"
+			idb.insert(dict_set,["komodo","datadir","data_files" ]) #,"password_on"
 			
 		elif not komodod_ok:
 			gui.messagebox_showinfo('Path for komodo deamon is wrong', deamon +'\n - no komodod file !')
 		elif  not komodo_cli_ok:
 			gui.messagebox_showinfo('Path for komodo-cli is wrong', data +'\n - no komodo-cli file !')
-		else:
+		elif not new_wallet:
 			gui.messagebox_showinfo('Path for blockchain data is wrong', data +'\n - no wallet file !')
 			
 	
@@ -223,32 +284,49 @@ class InitApp:
 
 	# qt dialog komodod, data dir 	
 	def ask_paths(self): # read from db if possible
+	
+		# self.data_files['wallet'], self.data_files['db']
+		tmpwallet=['Create new','Select file']
 
 		idb=localdb.DB('init.db')
 		
 		preset=[]
-		tt= idb.select('init_settings',columns=["komodo","datadir","start_chain"]) #,"password_on"
+		tt= idb.select('init_settings',columns=["komodo","datadir",'data_files']) #,"password_on" "start_chain",
 		if len(tt)>0:  
 
 			for t in tt[0]: # single row only
 				preset.append(t)
+				
+			if preset[2]!=None:
+				tmpj=json.loads(preset[2])
+				tmpwallet=[tmpj['wallet'] , 'Create new','Select file']
 		else:
 
-			preset=['','', 'no'] # preset win path
+			preset=['','', 'New' ] # preset win path "{'wallet':'wallet','db':'local_storage'}"
 			
 			curusr=getpass.getuser()
 			curpath=os.getcwd().split('\\') #C:\Users\zxcv\AppData\Roaming\Komodo\PIRATE
-			templatepath=curpath[0]+'/'+'/'.join(['Users',curusr,'AppData','Roaming','Komodo','PIRATE'])
+			templatepath=os.path.join(curpath[0],'Users',curusr,'AppData','Roaming','Komodo','PIRATE')
+			# curpath[0]+'/'+'/'.join(['Users',curusr,'AppData','Roaming','Komodo','PIRATE'])
 			
 			if sys.platform=='win32' and os.path.exists(templatepath):
 				preset[1]=templatepath
 			elif os.path.exists('/home/'+curusr+'/.komodo/PIRATE'):
 				preset[1]='/home/'+curusr+'/.komodo/PIRATE'
+				
+		# combobox actions:
+		# new - generate name
+		# select file - open dialog and select -> generate db name relevant
+		# other - select value 
+		# END: update 
 			
-		automate_rowids=[ [{'T':'LabelV', 'L':'Set proper paths, otherwise the deamon may freeze and you may need to kill the process manually.', 'span':3, 'width':120, 'style':{'bgc':'#eee','fgc':'red'}, 'uid':'none'},{  }, {  } ] ,
+			
+			
+		automate_rowids=[ [{'T':'LabelV', 'L':'Set proper paths, otherwise the deamon may freeze and you may need to kill the process manually.', 'span':3},{  }, {  } ] ,
 						[{'T':'LabelC', 'L':'Select deamon and cli path \n(komodod and komodo-cli inside)', 'width':32}, {'T':'Button','L':'Komodo path:','uid':'p1', 'width':15}, {'T':'LabelV', 'L':preset[0],'uid':'deamon', 'width':60} ],
 						[{'T':'LabelC', 'L':'Select data directory', 'width':32}, {'T':'Button','L':'Data dir path:','uid':'p2', 'width':15}, {'T':'LabelV', 'L':preset[1],'uid':'data', 'width':60} ] ,
-						[{'T':'LabelC', 'L':'Start blockchain', 'width':32}, {'T':'Combox','V':['no','yes'],'uid':'cs2', 'width':15 }, { } ],
+						[{'T':'LabelC', 'L':'Select wallet', 'width':32}, {'T':'Combox','V':tmpwallet,'uid':'cs2', 'width':15 }, { } ],
+						# [{'T':'LabelC', 'L':'Start blockchain', 'width':32}, {'T':'Combox','V':['no','yes'],'uid':'cs2', 'width':15 }, { } ],
 						[{'T':'Button','L':'Confirm','uid':'conf', 'span':3, 'width':120}, {   }, { } ]	] #, 'width':128
 			
 		tw=gui.Table( params={'dim':[5,3],"show_grid":False, 'colSizeMod':[256,'toContent','stretch'], 'rowSizeMod':['toContent','toContent','toContent','toContent','toContent']})		
@@ -257,9 +335,71 @@ class InitApp:
 		tw.cellWidget(1,1).set_fun(True,gui.set_file,tw.item(1,2),None,True,tw)
 		tw.cellWidget(2,1).set_fun(True,gui.set_file,tw.item(2,2),None,True,tw)
 		
-		def getv(tmptw):
+		
+		def combox(cbtn,tw):
+			if cbtn.currentText()=='Select file':
+				sel_file=gui.get_file_dialog('Select wallet file' ,init_path=preset[1],parent=tw,name_filter="Data (*.dat *.encr)")
+				sel_file=sel_file[0]
+				if sel_file=='':
+					return
+					
+				h,t=os.path.split(sel_file)	
+				
+				if len(t.split())>1:
+					gui.messagebox_showinfo('Wrong wallet file name', 'Wallet file name cannot contain spaces or white characters!')
+					return
+				
+				cbtn.addItem( t , t )
+				cbtn.setIndexForText( t)
+				
+		tw.cellWidget(3,1).every_click=True
+		tw.cellWidget(3,1).set_fun( combox,tw)
+		
+		
+		
+		def getv(tmptw):#self.data_files={'wallet':'wallet','db':'local_storage'}
+		
+			cbtntxt=tmptw.cellWidget(3,1).currentText()
+			data_path=tmptw.item(2,2).text()
+			# print(data_path)
+			
+			tmp_wal=''
+			newwallet=False
+		
+			if cbtntxt=='Create new':
+				tmprand=''
+				while True:
+					tmprand=self.cc.rand_slbls(3)
+					tmp_wal='wallet_'+tmprand+'.dat'
+					tmp_db='local_storage_'+tmprand+'.db'
+					if not os.path.exists(os.path.join(data_path,tmp_wal)) and not os.path.exists( os.path.join(os.getcwd(),tmp_db) ):
+						# print(os.path.join(data_path,tmp_wal), os.path.join(data_path,tmp_db) )
+						# with open(os.path.join(data_path,tmp_wal),'wb'):
+							# print('created new wallet file: ',os.path.join(data_path,tmp_wal))
+						break
+				self.data_files['db']='local_storage_'+tmprand
+				self.data_files['wallet']='wallet_'+tmprand
+				newwallet=True
+			
+			elif 'wallet_'==cbtntxt[:7] :
+				tmp=cbtntxt[7:].split('.')
+				self.data_files['db']='local_storage_'+tmp[0]
+				self.data_files['wallet']='wallet_'+tmp[0]
+			else:
+				tmp=cbtntxt.split('.')
+				self.data_files['wallet']=tmp[0]
+				
+				cbtntxt=cbtntxt.replace('wallet','')
+				tmp=cbtntxt.split('.')
+				if tmp[0]=='':
+					self.data_files['db']= 'local_storage'
+				else:
+					self.data_files['db']= 'local_storage_'+tmp[0] 
+				
+			# print(self.data_files)
+				
 			self.paths_confirmed=True
-			self.update_paths(tmptw.item(1,2).text(),  tmptw.item(2,2).text(), tmptw.cellWidget(3,1).currentText() ,tmptw)
+			self.update_paths(tmptw.item(1,2).text(),  tmptw.item(2,2).text(), json.dumps(self.data_files), tmptw,newwallet)
 			tmptw.parent().close()
 
 		tw.cellWidget(4,0).set_fun(True, getv,tw)
@@ -272,25 +412,25 @@ class InitApp:
 	
 
 	
-	
+	# self.data_files['db']
 	def isvalid(self,pas):
 
 		ppath=os.getcwd()
 		
-		if os.path.exists(os.path.join(ppath , 'local_storage.encr')):
+		if os.path.exists(os.path.join(ppath , self.data_files['db']+'.encr')):
 			# print(256,pas)
 			# print('decr path',os.path.join(ppath , 'local_storage.encr'), os.path.join(ppath,'local_storage.db'))
-			self.cc.aes_decrypt_file( os.path.join(ppath , 'local_storage.encr'), os.path.join(ppath,'local_storage.db') , pas)
+			self.cc.aes_decrypt_file( os.path.join(ppath , self.data_files['db']+'.encr'), os.path.join(ppath,self.data_files['db']+'.db') , pas)
 			
-			db=localdb.DB( )
+			db=localdb.DB(self.data_files['db']+'.db')
 			at=db.all_tables()
 			# print(at)
 			if len(at)>0:
-				app_fun.secure_delete(os.path.join(ppath , 'local_storage.encr'))
+				app_fun.secure_delete(os.path.join(ppath , self.data_files['db']+'.encr'))
 
 				return True
 				
-		elif os.path.exists(os.path.join(ppath , 'local_storage.db')):
+		elif os.path.exists(os.path.join(ppath , self.data_files['db']+'.db')):
 			return True
 			
 		return False
@@ -329,27 +469,26 @@ class InitApp:
 			tryii=tryii-1
 			try:
 			# if True:
-				self.cc.aes_encrypt_file( os.path.join(ppath ,'local_storage.db'), os.path.join(ppath ,'local_storage.encr') , self.app_password)
+				self.cc.aes_encrypt_file( os.path.join(ppath ,self.data_files['db']+'.db'), os.path.join(ppath ,self.data_files['db']+'.encr') , self.app_password)
 				
-				if os.path.exists(os.path.join(ppath ,'local_storage.encr')):
+				if os.path.exists(os.path.join(ppath ,self.data_files['db']+'.encr')):
 					
-					app_fun.secure_delete(os.path.join(ppath ,'local_storage.db'))
+					app_fun.secure_delete(os.path.join(ppath ,self.data_files['db']+'.db'))
 				
-					gui.messagebox_showinfo("local_storage.db encrypted", "DB secure: local_storage.db -> local_storage.encr ",parent)
+					gui.messagebox_showinfo("Database encrypted", "DB secure: "+self.data_files['db']+".db -> "+self.data_files['db']+".encr ",parent)
 			
 					break
 			except:
-				print('Exception in delete local_storage.db', tryii)
+				print('Exception in delete '+self.data_files['db']+'.db', tryii)
 			
 			time.sleep(1)
-			
 			
 			
 			
 
 	def on_closing(self,parent):
 		# self.close_thread=True
-		db=localdb.DB( )
+		db=localdb.DB(self.data_files['db']+'.db' )
 		db.vacuum()
 		is_deamon_working=app_fun.check_deamon_running()
 		if is_deamon_working[0]:
