77219ebef4900dfb3baa1e842d9f989ca5ff317a
buidl1 <71228185+buidl1@users.noreply.github.com>
2022-02-21 20:44:53 +0100

    Update tx_history.py

--- modules/tx_history.py
+++ modules/tx_history.py
@@ -1,4 +1,7 @@
 
+
+
+
 # import tkinter as tk
 # from tkinter import filedialog, StringVar, ttk, messagebox, Toplevel 
 import os
@@ -28,7 +31,8 @@ class TransactionsHistory:
 		self.update_in_progress=False
 		self.parent_frame = gui.ContainerWidget(None,layout=gui.QVBoxLayout() )
 		self.db=db
-		idb=localdb.DB(self.db)
+		# idb=localdb.DB(self.db)
+		# task_done=idb.select('tx_history', ['Category','Type','status','txid','block','date_time','from_str','to_str','amount','uid'],where=wwhere,orderby=[{'block':'desc'},{'Type':'asc'},{'timestamp':'desc'}])
 		
 		frame0=gui.FramedWidgets(None,'Filter') #ttk.LabelFrame(parent_frame,text='Filter')  
 		frame0.setMaximumHeight(128)
@@ -60,7 +64,7 @@ class TransactionsHistory:
 
 		self.update_list()
 		
-		self.main_table=gui.Table(None,params={'dim':[len(self.grid_settings),len(self.colnames)],'updatable':1} )
+		self.main_table=gui.Table(None,params={'dim':[len(self.grid_settings),len(self.colnames)],'updatable':1,'default_sort_col':'Date time'} )
 		frame1.insertWidget(self.main_table)     
 		self.main_table.updateTable(self.grid_settings,self.colnames)
 		
@@ -88,25 +92,41 @@ class TransactionsHistory:
 		elif llast=='12 months':  
 			wwhere['timestamp']=['>=',str( (datetime.datetime.now()-datetime.timedelta(days=365) ).timestamp() ) ]
 		
-		ccat=self.filter_table.cellWidget(0,1).currentText() #get_value('category')
-		rres=self.filter_table.cellWidget(0,3).currentText() #.get_value('result')
-		cmd=self.filter_table.cellWidget(0,0).currentText() #.get_value('command')
+		# ['Category','Type','Last','Status']
+		ccat=self.filter_table.cellWidget(0,1).currentText() #get_value('category') {'T':'Combox' , 'V':['All','in','out'] } 
+		rres=self.filter_table.cellWidget(0,3).currentText() #.get_value('result')  {'T':'Combox',  'V':['All','sent','received','notarized' ] }
+		cmd=self.filter_table.cellWidget(0,0).currentText() #.get_value('command') ['All','send','merge','other']
 		
 		self.grid_settings=[]
 		
+		# print('ccat,rres,cmd',)
 		if rres!='All':
-			wwhere['Type']=['=',"'"+rres+"'"]
+			# wwhere['Type']=['=',"'"+rres+"'"]
+			wwhere['status']=['=',"'"+rres+"'"]
+			
 		if ccat!='All':
-			wwhere['Category']=['=',"'"+ccat+"'"]
+			wwhere['Type']=['=',"'"+ccat+"'"]
+			# wwhere['Category']=['=',"'"+ccat+"'"]
+			# if ccat=='other':
+				# wwhere['Category']=[' not in ',"('send','merge')"]
+		if cmd!='All':
+			# wwhere['status']=['=',"'"+cmd+"'"]
+			wwhere['Category']=['=',"'"+cmd+"'"]
 			if ccat=='other':
 				wwhere['Category']=[' not in ',"('send','merge')"]
-		if cmd!='All':
-			wwhere['status']=['=',"'"+cmd+"'"]
 		
-		
+		# print('wwhere',wwhere)
 		task_done=idb.select('tx_history', ['Category','Type','status','txid','block','date_time','from_str','to_str','amount','uid'],where=wwhere,orderby=[{'block':'desc'},{'Type':'asc'},{'timestamp':'desc'}])
 		
+		# self.filter_table.cellWidget(0,1).set_fun(self.update_history_frame) # update filter based on this 
+		dist_types=['All','in','out']
+		tmp_cut_selection=self.filter_table.cellWidget(0,1).text()
+		
 		for ij,rr in enumerate(task_done):
+		
+			# print('tx_history',rr)
+			if rr[1] not in dist_types: dist_types.append(rr[1])
+			
 			tmpdict={}
 			sstyle={'bgc':'green','fgc':'#fff'}
 			if 'sent' in rr[2].lower() or 'received' in rr[2].lower():
@@ -114,13 +134,14 @@ class TransactionsHistory:
 							
 			visible=True
 			tmpdict['rowk']=rr[9]
+			from_str=rr[6].replace('\0','')
 			tmpdict['rowv']=[{'T':'LabelV', 'L':rr[0], 'uid':'cat'+str(rr[9]), 'visible':visible } , 
 							{'T':'LabelV', 'L':rr[1], 'uid':'type'+str(rr[9]) , 'visible':visible} , 
 							{'T':'LabelV', 'L':rr[2], 'uid':'stat'+str(rr[9]) , 'visible':visible, 'style':sstyle} , 
 							{'T':'LabelV', 'L':str(rr[4]), 'uid':'block'+str(rr[9]), 'visible':visible  } ,
 							{'T':'LabelV', 'L': rr[5]  , 'uid':'ts'+str(rr[9]) , 'visible':visible} , 
 							{'T':'LabelV', 'L':str(rr[8]), 'uid':'am'+str(rr[9]) , 'visible':visible} , 
-							{'T':'LineEdit', 'V': rr[6], 'uid':'from'+str(rr[9]), 'visible':visible, 'width':24} , 
+							{'T':'LineEdit', 'V': from_str, 'uid':'from'+str(rr[9]), 'visible':visible, 'width':24} , 
 							{'T':'LineEdit', 'V': rr[7], 'uid':'to'+str(rr[9]) , 'visible':visible, 'width':24} , 
 							{'T':'LineEdit', 'V': rr[3] , 'uid':'txid'+str(rr[9]) , 'visible':visible, 'width':6} 
 							]	
@@ -128,3 +149,5 @@ class TransactionsHistory:
 							
 			self.grid_settings.append(tmpdict)
 			
+		self.filter_table.cellWidget(0,1).updateBox( dist_types)
+		self.filter_table.cellWidget(0,1).setIndexForText(tmp_cut_selection)
