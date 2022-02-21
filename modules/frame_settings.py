eb8cf4190468584ba9d613a04f93e3637529652f
buidl1 <71228185+buidl1@users.noreply.github.com>
2022-02-21 20:42:01 +0100

    Update frame_settings.py

--- modules/frame_settings.py
+++ modules/frame_settings.py
@@ -2,18 +2,19 @@
 
 import os
 # import datetime
-# import json
+import json
 # import time
 import modules.localdb as localdb
-# import modules.app_fun as app_fun
+import modules.app_fun as app_fun
 # import operator
 # import modules.flexitable as flexitable
 import modules.gui as gui
+import modules.aes as aes
 
 class Settings:
 
 
-	def __init__(self, wds):
+	def __init__(self, wds, initApp):
 		# global idb
 		self.db=wds.db
 		self.grid_settings=[]
@@ -25,16 +26,34 @@ class Settings:
 		def clear_change_info(*evargs):
 			newpass=self.pass_table.cellWidget(0,1).text().strip()
 			oldpass=self.pass_table.cellWidget(0,0).text().strip()
+			
 			if oldpass==wds.password:
-				wds.password=newpass
-				# clear
-				self.pass_table.cellWidget(0,0).setText('')
-				self.pass_table.cellWidget(0,1).setText('')
-				# info
-				
-				gui.showinfo("Password changed", "You have new password! Leading and trailing spaces where removed from the password to avoid mistakes.",parent_frame )
+				if oldpass==newpass:
+					gui.showinfo("WARNING - Password NOT changed", "Old and new passwords are the same!",self.parent_frame )
+					
+					
+				else:	
+					wds.password=newpass
+					initApp.app_password=newpass
+					
+					db=localdb.DB(self.db )
+					cc=aes.Crypto()
+					table={} 
+					salttmp= cc.init_hash_seed() 
+					passhashtmp= cc.hash(newpass+salttmp) 
+					disp_dict={'salt':salttmp,'passhash':passhashtmp} # 
+					table['jsons']=[{'json_name':"password_hash", 'json_content':json.dumps(disp_dict), 'last_update_date_time': app_fun.now_to_str(False)}]
+					db.upsert(table,['json_name','json_content','last_update_date_time'],{'json_name':['=',"'password_hash'"]})
+
+					
+					# clear
+					self.pass_table.cellWidget(0,0).setText('')
+					self.pass_table.cellWidget(0,1).setText('')
+					# info
+					
+					gui.showinfo("Password changed", "You have new password! Leading and trailing spaces where removed from the password to avoid mistakes.",self.parent_frame )
 			else:
-				gui.showinfo("WARNING - Password NOT changed", "Your old password typed incorectly! Try again!",parent_frame )
+				gui.showinfo("WARNING - Password NOT changed", "Your old password typed incorrectly! Try again!",self.parent_frame )
 		
 		# self.password.setEchoMode(QLineEdit.Password)
 		def toggle_pass_view( btn):
@@ -56,11 +75,6 @@ class Settings:
 							, {'T':'Button', 'L':'Submit', 'fun': clear_change_info}
 							]
 		grid_pass.append(tmpdict )	
-		# tmpdict={}			
-		# tmpdict['rowk']='note'
-		# tmpdict['rowv']=[ {'T':'LabelV','L':'To change password you need to have wallet synced!',  'span':4 }							]
-		
-		# grid_pass.append(tmpdict )
 		
 		self.pass_table=gui.Table(frame0,params={'dim':[1,4], 'toContent':1} )       #flexitable.FlexiTable(frame0,grid_pass) # +++ update command filter after each iteration
 		self.pass_table.updateTable(grid_pass,colnames)
@@ -69,18 +83,12 @@ class Settings:
 		frame0.insertWidget(self.pass_table)
 		self.updatePassChangeState()
 		
-		# self.pass_table.set_cmd('enter',[], clear_change_info )	
-		# self.pass_table.cellWidget(1,3).set_fun(True,clear_change_info)
-		
-		
 		
 		################ DB maintenance / clear old tasks {'total_chars':total_chars, 'total_rows':rowsii, 'older_chars':older_chars, 'old_rows':old_rows}
 		
 		# idb=localdb.DB(self.db)
 		frame1 = gui.FramedWidgets(None,'DB maintenance',layout=gui.QHBoxLayout())
 		self.parent_frame.insertWidget(frame1)
-		# frame1=ttk.LabelFrame(parent_frame,text='DB maintenance')  
-		# frame1.grid(row=0,column=1, sticky="nsew")
 		
 		def update_db_info():
 			idb=localdb.DB(self.db)
@@ -88,34 +96,33 @@ class Settings:
 			grid_db= []
 			colnames=['Table name','Table size','' ]
 			
-			tmpsize=idb.table_size('jsons')
+			tmpsize_multi=idb.table_size(['jsons','tx_history','notifications','queue_done'])
+			 # {'jsons': {'total_chars': 4645, 'total_rows': 6, 'older_chars': 0, 'old_rows': 0}, 'tx_history': {'total_chars': 0, 'total_rows': 0, 'older_chars': 0, 'old_rows': 0}}
+			# print('tmpsize_multi',tmpsize_multi)
+			
+			tmpsize=tmpsize_multi['jsons'] #idb.table_size('jsons')
 			tmpdict={}
 			tmpdict['rowk']='json'
 			tmpdict['rowv']=[{ 'T':'LabelV', 'L':'[jsons]'}, { 'T':'LabelV', 'L': str(tmpsize['total_chars'])+' chars / '+str(tmpsize['total_rows'])+' rows '}]
 			# tmpdict['jsons']=[{'uid':'4','T':'LabelV', 'L':'[jsons] table size: '+str(tmpsize['total_chars'])+' chars '+str(tmpsize['total_rows'])+' rows '}]
 			grid_db.append(tmpdict)
 			
-			tmpsize=idb.table_size('tx_history')
+			tmpsize=tmpsize_multi['tx_history'] #idb.table_size('tx_history')
 			tmpdict={}
 			tmpdict['rowk']='tx_history'
 			tmpdict['rowv']=[{ 'T':'LabelV', 'L':'[tx_history]'}, { 'T':'LabelV', 'L': str(tmpsize['total_chars'])+' chars / '+str(tmpsize['total_rows'])+' rows '}]
 			grid_db.append(tmpdict)
 			
 			
-			tmpsize=idb.table_size('notifications')
+			tmpsize=tmpsize_multi['notifications'] #idb.table_size('notifications')
 			tmpdict={}
 			tmpdict['rowk']='db_clear_old_notif'
 			tmpdict['rowv']=[{ 'T':'LabelV', 'L':'[notifications]'}, { 'T':'LabelV', 'L': str(tmpsize['total_chars'])+' chars / '+str(tmpsize['total_rows'])+' rows '},{'T':'Button', 'L':'Delete older then 1 day','span':2  }]
 			grid_db.append(tmpdict)
 			
-			# tmpdict={}
-			# tmpdict['rowk']='db_clear_old_notif2'
-			# tmpdict['rowv']=[]
-			# grid_db.append(tmpdict)
 			
 			
-			
-			tmpsize=idb.table_size('queue_done')
+			tmpsize=tmpsize_multi['queue_done'] #idb.table_size('queue_done')
 			tmpdict={}
 			tmpdict['rowk']='db_clear_old_tasks'
 			tmpdict['rowv']=[{ 'T':'LabelV', 'L':'[tasks]'}, { 'T':'LabelV', 'L': str(tmpsize['total_chars'])+' chars / '+str(tmpsize['total_rows'])+' rows '}]
@@ -150,6 +157,8 @@ class Settings:
 		
 		# return parent_frame
 		
+	@gui.Slot()	
+	@gui.Slot(bool)	
 	def updatePassChangeState(self,enable=False):
 		
 		disable=True-enable
