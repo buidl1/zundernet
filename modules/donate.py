c3a4ac87be9ebba8e96968c3beabcef5e6aa48e1
buidl1 <71228185+buidl1@users.noreply.github.com>
2022-02-23 19:51:20 +0100

    Update donate.py

--- modules/donate.py
+++ modules/donate.py
@@ -3,7 +3,7 @@ import modules.gui as gui
 
 def donate(parent_frame,wds,verus=False):
 		
-	addr='zs1wa9kly0u6g0pyjmruwsvxql2wxx8hdwtrr0qyenla5y5m2vyc8nrtxq9s2dy3cv54cjrjzdne6w'
+	addr='zs1pda8pf379rycyqlhcv5g0v8rua06a64s90lncs9xawrjt7crss7sgzmls924ujj2tqcz6evhft4'
 	llabel='Donate 1 Pirate'
 	if verus:
 		addr='zs1gezt5nne5lm94q3cvrrg7fxke668w7sjced0s90a5zetp0v5ndxa7sd4n8y7usqavl8mg898yhr'
